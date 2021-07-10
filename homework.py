3299a6b0584cf758a5a9c2fa807ef35eb688a537
Bolatbek <abdukerimovbn@yandex.ru>
2021-07-10 18:54:54 +0300

    Ver. 1

--- homework.py
+++ homework.py
@@ -1,9 +1,10 @@
+import logging
 import os
 import time
+
 import requests
 import telegram
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
@@ -11,15 +12,12 @@ PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-# проинициализируйте бота здесь,
-# чтобы он был доступен в каждом нижеобъявленном методе,
-# и не нужно было прокидывать его в каждый вызов
-bot = ...
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework['homework_name']
+    if homework['status'] == 'rejected':
         verdict = 'К сожалению, в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, работа зачтена!'
@@ -27,12 +25,15 @@ def parse_homework_status(homework):
 
 
 def get_homeworks(current_timestamp):
-    homework_statuses = ...
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    params = {'from_date': current_timestamp}
+    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+    homework_statuses = requests.get(url=url, params=params, headers=headers)
     return homework_statuses.json()
 
 
 def send_message(message):
-    return bot.send_message(...)
+    return bot.send_message(chat_id=CHAT_ID, text='Done')
 
 
 def main():
@@ -40,13 +41,25 @@ def main():
 
     while True:
         try:
-            ...
+            homework_req = get_homeworks(current_timestamp)
+            if homework_req.get('homeworks'):
+                send_message(parse_homework_status(
+                    homework_req.get('homework')[0])
+                )
+            current_timestamp = homework_req.get('current_date')
             time.sleep(5 * 60)  # Опрашивать раз в пять минут
 
         except Exception as e:
             print(f'Бот упал с ошибкой: {e}')
             time.sleep(5)
+            continue
 
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename='main.log',
+    format='%(asctime)s, %(levelname)s, %(name)s, %(massage)s'
+)
 
 if __name__ == '__main__':
     main()
